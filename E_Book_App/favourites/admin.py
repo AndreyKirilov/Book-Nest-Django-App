f23f76d5e694673fd8c159af26dad408cd2b90c8
AndreyKirilov <145570409+AndreyKirilov@users.noreply.github.com>
2024-12-11 17:34:49 +0200

    Fixed error and added functionality

--- E_Book_App/favourites/admin.py
+++ E_Book_App/favourites/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Favourites
 
-# Register your models here.
+
+@admin.register(Favourites)
+class FavouritesAdmin(admin.ModelAdmin):
+    list_display = ['user', 'book', 'created_on']
+    list_filter = ['user', ]
--- E_Book_App/favourites/models.py
+++ E_Book_App/favourites/models.py
@@ -10,3 +10,6 @@ class Favourites(models.Model):
     user = models.ForeignKey(AppUserModel, on_delete=models.CASCADE, related_name='user_favourites')
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'book')
